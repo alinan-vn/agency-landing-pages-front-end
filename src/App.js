fd93f4b1d1f1119e649a5b4ca95c6e4744880821
alinan-vn <jandrolinan@gmail.com>
2020-06-10 13:15:21 -0500

    Pages created and connected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/main.scss';
+import Nav from './components/global/nav/index';
+import Landing from './components/landing_page/index';
+import About from './components/about/index';
+import Projects from './components/projects/index';
+import Staff from './components/staff/index';
+import Contact from './components/contact_us/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path ='/' component={Landing} />
+        <Route exact path ='/about' component={About} />
+        <Route exact path ='/projects' component={Projects} />
+        <Route exact path ='/staff' component={Staff} />
+        <Route exact path ='/contact-us' component={Contact} />
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
