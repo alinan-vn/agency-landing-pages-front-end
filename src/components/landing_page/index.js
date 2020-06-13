111d8ecc9d8b7a4599230fa1c1b19b94a294f227
alinan-vn <jandrolinan@gmail.com>
2020-06-12 19:00:08 -0500

    landing page structure and styling

--- src/components/landing_page/index.js
+++ src/components/landing_page/index.js
@@ -6,7 +6,7 @@ class Landing extends React.Component {
         return(
             <main>
                 <section className='landing'>
-                    <h1>Researching and Creating for a better tomorrow.</h1>
+                    <h1 className='landing__text'>Researching and Creating for a better tomorrow.</h1>
                 </section>
             </main>
         )
