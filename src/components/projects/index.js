79394403ce371b3933b48c7e9d2a773820571573
alinan-vn <jandrolinan@gmail.com>
2020-06-15 09:03:38 -0500

    some project structure and styling

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 
 class Projects extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            projectImages: [
+                '../../img/project_images/example.jpg',
+                '../place one', 
+                '../asdfasdf'
+            ]
+        }
+    }
 
     render(){
         return(
             <main>
                 <section className='projects'>
-                    <h1 className='projects__text'>Projects</h1>
+                    <div className='projects__landing-image'>
+                        <h3 className='projects__header-text'>
+                            Here, peacefully, lie our projects may they rest in peace. Take a gander and see what you like ;)
+                        </h3>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={require('../../img/project_images/example.jpg')} alt='example' />
+                            <p>image should be here</p>
+                        </div>
+                        <div>
+                            <img src={require('../../img/project_images/example.jpg')} alt='example' />
+                            <p>image should be here</p>
+                        </div>
+                        <div>
+                            <img src={require('../../img/project_images/example.jpg')} alt='example' />
+                            <p>image should be here</p>
+                        </div>
+                    </div>
                 </section>
             </main>
         )
