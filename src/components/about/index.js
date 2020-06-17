a24904f92b32e42143ef4d187f092deda34bfc09
Linda <ltramos7@gmail.com>
2020-06-17 17:22:32 -0500

    added Philosophy and Our Team template in About section

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -2,26 +2,40 @@ import React, { Component } from 'react';
 
 class About extends React.Component {
 
+
     render() {
         return (
             <main className="about">
-                <div className="about-box">
-                    <h2 className="h-2">About Us</h2>
+                <div className="trans-box">
+                    <h2>About Us</h2>
                 </div>
 
-                <div>
-                    <h3>We are team that wants to provide quality work that will make your business successful.</h3>
+                <div className="content-left">
+                    <h3 >Our Philosophy</h3>
+
+                    <p>
+                        Do dolor eiusmod adipisicing ad ullamco sint eu proident. Est  duis sunt anim et. Ea dolore nulla aliqua proident tempor. Nostrud aliquip consectetur et officia eu dolor ipsum pariatur aute esse.
+                    </p>
+
+                    <p>
+                        Magna officia officia et eu cillum Lorem veniam ad excepteur mollit incididunt. Officia ut duis sit aliquip est aliqua sint eiusmod ad voluptate labore. Ex mollit veniam laborum amet in laborum nulla velit. Consectetur velit dolore fugiat eiusmod ipsum non nostrud exercitation non fugiat commodo do fugiat exercitation.
+                    </p>
+
+                    <p>
+                        We offer a wide variety of services from Logo create to web apps.
+                    </p>
                 </div>
 
-                <div>
-                    <p>We are a team of many backgrounds who collaborate with each other through the whole process of your product.</p>
+                <div className="content-right">
+                    <h3 >Our Team</h3>
+                    <p>Nisi quis amet Lorem aliqua magna aute voluptate esse nostrud.</p>
 
-                    <p>We offer a wide variety of services from Logo create to web apps.</p>
+                    <p>Ea irure elit occaecat et consectetur minim magna esse dolor occaecat. Mollit nostrud qui consequat labore exercitation consectetur deserunt est eiusmod consequat reprehenderit laborum deserunt. Lorem commodo commodo non cillum anim nisi culpa cillum qui incididunt nulla deserunt fugiat do.</p>
+
+                    {/* <p>
+                        We are a team of many backgrounds who collaborate with each other through the whole process of your product.
+                    </p> */}
                 </div>
-
-
-
-
             </main>
         )
     }
